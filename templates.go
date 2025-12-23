aefe0dc60f86dad2c3718ed78edec153ebedc596
Vegard Stikbakke <vegard.stikbakke@gmail.com>
2025-12-23 11:22:12 +0100

    Add social links

--- templates.go
+++ templates.go
@@ -151,6 +151,8 @@ var baseTemplate = `<!DOCTYPE html>
         <!-- TODO: Uncomment to show books again
         <a href="/books/">Books</a>
         -->
+        <a href="https://github.com/vegarsti">GitHub</a>
+        <a href="https://twitter.com/vegardstikbakke">Twitter</a>
     </nav>
     <main>
         {{template "content" .}}
